afbc5171dd7ed6f2276576fcb0d4a9a25a31cc2c
Gabegual <Roblox4@gmail.com>
2020-10-26 19:13:32 -0400

    im sooo mad this was it

---
+++ src/CoffeeTouchscreenAdapter.java
@@ -0,0 +1,23 @@
+
+public class CoffeeTouchscreenAdapter implements CoffeeMachineInterface {
+	
+	OldCoffeeMachine  OldVendingMachine;
+
+	@Override
+	public void chooseFirstSelection() {
+		
+		
+	}
+
+	@Override
+	public void chooseSecondSelection() {
+		
+		
+	}
+
+
+
+
+
+
+}
---
+++ src/OldCoffeeMachine.java
@@ -0,0 +1,12 @@
+
+public class OldCoffeeMachine {
+	
+	public void selectA() {
+		System.out.println("Option:A");
+	}
+	public void selectB() {
+		System.out.println("Option:B");
+	}
+
+}
+
